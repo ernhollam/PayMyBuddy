458ed8fa3a621a0c192b91ec41339926a814c728
ernhollam <ernholla.marinasy@gmail.com>
2022-10-10 16:20:44 +0200

    Implement deposit and withdrawal features. Add title to alerts

--- src/main/java/com/paymybuddy/paymybuddy/controller/ProfileController.java
+++ src/main/java/com/paymybuddy/paymybuddy/controller/ProfileController.java
@@ -9,7 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -17,24 +20,51 @@ import java.util.List;
 @RequestMapping("/profile")
 public class ProfileController {
 
-    @Autowired
-    private UserService        userService;
-    @Autowired
-    private TransactionService transactionService;
-    @Autowired
-    private ConnectionService  connectionService;
+	@Autowired
+	private UserService        userService;
+	@Autowired
+	private TransactionService transactionService;
+	@Autowired
+	private ConnectionService  connectionService;
 
-    @GetMapping
-    public String showProfilePage(Model model) {
+	@GetMapping
+	public String showProfilePage(Model model) {
 
-        User                connectedUser = userService.getCurrentUser();
-        List<UserViewModel> connections   = connectionService.getUserConnections(connectedUser);
+		User                connectedUser = userService.getCurrentUser();
+		List<UserViewModel> connections   = connectionService.getUserConnections(connectedUser);
 
-        model.addAttribute("user", connectedUser);
-        model.addAttribute("balance", connectedUser.getBalance());
-        model.addAttribute("page", "profile");
-        model.addAttribute("connections", connections);
+		model.addAttribute("user", connectedUser);
+		model.addAttribute("balance", connectedUser.getBalance());
+		model.addAttribute("page", "profile");
+		model.addAttribute("connections", connections);
 
-        return "profile";
-    }
+		return "profile";
+	}
+
+	@GetMapping("/update-balance")
+	public String showUpdateBalancePage(Model model) {
+		model.addAttribute("page", "update-balance");
+		return "update-balance";
+	}
+
+	@PostMapping("/update-balance")
+	public String updateBalance(@RequestParam String action, double amount, Model model, RedirectAttributes redirAttrs) {
+		try {
+			String strAmount = String.valueOf(amount);
+			switch (action) {
+				case "deposit" -> {
+					userService.deposit(userService.getCurrentUser(), strAmount);
+				}
+				case "withdrawal" -> {
+					userService.withdraw(userService.getCurrentUser(), strAmount);
+				}
+			}
+			redirAttrs.addFlashAttribute("success",
+					"Your "+ action + " of " + strAmount + "€ was successful!");
+		} catch (Exception e) {
+			redirAttrs.addFlashAttribute("error", e.getMessage());
+			return "redirect:/update-balance";
+		}
+		return "redirect:/profile";
+	}
 }
