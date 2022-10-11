2dc48bd25cf2d9cba3428b1c8c56fa95c25d9669
ernhollam <ernholla.marinasy@gmail.com>
2022-10-11 16:31:12 +0200

    Add spring security dependency

--- src/main/java/com/paymybuddy/paymybuddy/config/ClockConfig.java
+++ src/main/java/com/paymybuddy/paymybuddy/config/ClockConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import java.time.Clock;
 
 @Configuration
-public class PayMyBuddyConfiguration {
+public class ClockConfig {
 	/**
 	 * Creates a Clock bean to have system's default zone for LocalDate.now().
 	 * @return a clock.
