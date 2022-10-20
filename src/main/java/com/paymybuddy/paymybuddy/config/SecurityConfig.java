df27617a2b46d055d049c173ec2476187de4d186
e.marinasy <ernholla.marinasy@lomaco.fr>
2022-10-20 16:06:05 +0200

    Fix name for password input in login.html

--- src/main/java/com/paymybuddy/paymybuddy/config/SecurityConfig.java
+++ src/main/java/com/paymybuddy/paymybuddy/config/SecurityConfig.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -26,7 +29,6 @@ public class SecurityConfig {
 	public UserDetailsService userDetailsService() {
 		return new DBUserDetailsService(userRepository);
 	}
-
 	@Bean
 	public DaoAuthenticationProvider authenticationProvider() {
 		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -36,27 +38,27 @@ public class SecurityConfig {
 		return authProvider;
 	}
 
-	/*@Bean
+	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity
+				.authenticationProvider(authenticationProvider())
 				.authorizeHttpRequests((requests) -> requests
-						.mvcMatchers("/login", "/signup").permitAll())
-						//.anyRequest().authenticated())
+						.mvcMatchers("/login", "/signup").permitAll()
+						.anyRequest().authenticated())
 				.formLogin((form) -> form
 						.loginPage("/login")
 						.usernameParameter("email")
 						.passwordParameter("password")
-						.defaultSuccessUrl("/home", true))
+						.defaultSuccessUrl("/", true))
 				.rememberMe(rememberMeConfigurer -> rememberMeConfigurer.userDetailsService(userDetailsService()))
 				.logout((logout) -> logout
 						.logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
 						.logoutSuccessUrl("/login?logout")
 						.invalidateHttpSession(true)
 						.deleteCookies("JSESSIONID"));
-		;
 
 		return httpSecurity.build();
-	}*/
+	}
 
 	@Bean
 	public WebSecurityCustomizer webSecurityCustomizer() {
