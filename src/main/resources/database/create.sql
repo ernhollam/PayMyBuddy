b1818c101d6057c85b04a5735a7f977ed6ba6505
e.marinasy <ernholla.marinasy@lomaco.fr>
2022-10-31 11:53:59 +0100

    Add not null in user table and set foreign keys as primary keys in connection and transaction table.

--- src/main/resources/database/create.sql
+++ src/main/resources/database/create.sql
@@ -6,14 +6,26 @@ USE db_paymybuddy;
 CREATE TABLE user (
     user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(100),
-    firstname VARCHAR(50),
-    lastname VARCHAR(50),
-    balance DECIMAL(10, 2)
+    password VARCHAR(100) NOT NULL,
+    firstname VARCHAR(50) NOT NULL,
+    lastname VARCHAR(50) NOT NULL,
+    balance DECIMAL(10, 2) NOT NULL DEFAULT 0.0
+);
+
+CREATE TABLE bank_account(
+    account_id INT NOT NULL AUTO_INCREMENT,
+    fk_user_id INT NOT NULL,
+    bank_name VARCHAR(100) DEFAULT NULL,
+    iban VARCHAR(34) DEFAULT NULL,
+    balance DECIMAL(20,2) NOT NULL DEFAULT 0.0,
+    PRIMARY KEY (account_id, fk_user_id),
+    FOREIGN KEY (fk_user_id)
+        REFERENCES user (user_id)
+        ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
 CREATE TABLE connection (
-    connection_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    connection_id INT NOT NULL AUTO_INCREMENT,
     fk_initializer_id INT NOT NULL,
     fk_receiver_id INT NOT NULL,
     starting_date DATETIME NOT NULL,
@@ -22,11 +34,12 @@ CREATE TABLE connection (
         ON DELETE NO ACTION ON UPDATE NO ACTION,
     FOREIGN KEY (fk_receiver_id)
         REFERENCES user (user_id)
-        ON DELETE NO ACTION ON UPDATE NO ACTION
+        ON DELETE NO ACTION ON UPDATE NO ACTION,
+    PRIMARY KEY (connection_id, fk_initializer_id, fk_receiver_id)
 );
 
 CREATE TABLE transaction (
-    transaction_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    transaction_id INT NOT NULL AUTO_INCREMENT,
     fk_issuer_id INT NOT NULL,
     fk_payee_id INT NOT NULL,
     date DATETIME NOT NULL,
@@ -37,5 +50,6 @@ CREATE TABLE transaction (
         ON DELETE NO ACTION ON UPDATE NO ACTION,
     FOREIGN KEY (fk_payee_id)
         REFERENCES user (user_id)
-        ON DELETE NO ACTION ON UPDATE NO ACTION
+        ON DELETE NO ACTION ON UPDATE NO ACTION,
+    PRIMARY KEY (transaction_id, fk_issuer_id, fk_payee_id)
 );
